15e99c3638698300781adfa6923d51571afc9912
lautarocuneo <lautarocuneo@gmail.com>
2024-11-25 17:21:24 -0300

    xd

--- challenge/backend/index.js
+++ challenge/backend/index.js
@@ -1,27 +1,29 @@
 const express = require("express");
 const mongoose = require('mongoose');
 const cors = require("cors");
-const { SECRET_JWT_KEY } = require('./config');
-const jwt = require('jsonwebtoken');
-const UserModel = require('./models/Table');
+const { SECRET_JWT_KEY } = require('./config'); //me traigo la secret key
+const jwt = require('jsonwebtoken'); //importa jsonwebtoken libreria para generar y verificar tokens para autenticacion
+const UserModel = require('./models/Table'); //me traigo la data de como seran almacenados los datos en la base de datos
 
-const app = express();
-app.use(express.json());
-app.use(cors());
+const app = express(); //instancio la aplicacion express
+app.use(express.json()); //admite formato json 
+app.use(cors()); //permite ue la api acepte solicitudes de otros dominios, comunicacion entre backend y frontend
 
 
-const mongoURI = process.env.MONGO_URI || "mongodb://mongodb:27017/challengedatabase";
+const mongoURI = process.env.MONGO_URI || "mongodb://mongodb:27017/challengedatabase"; //es la uri por defecto para conectarse a una base de datos mongodb local o contenedor docker
 
-mongoose.connect(mongoURI, { //conecto a la base de datos
-    useNewUrlParser: true,
+mongoose.connect(mongoURI, { //conecto a la base de datos usando la uri definida arriba
+    useNewUrlParser: true, //funcionalidades mongodb
     useUnifiedTopology: true
 }).then(() => {
-    console.log('MongoDB connected successfully');
+    console.log('MongoDB connected successfully'); //conectamos de manera exitosa
 }).catch(err => {
-    console.error('MongoDB connection error:', err);
+    console.error('MongoDB connection error:', err); //hubo error
 });
 
-app.post('/login', (req, res) => {
+
+//app.post este codigo se ejecuta cuando recibo una solicitud post en mi puerto 3001 en la ruta /login 
+app.post('/login', (req, res) => { //req tiene los datos enviados por el cliente, res es lo que devolvemoss
     const {email, password} = req.body; //me guardo el objeto que me mando el cliente
     UserModel.findOne({email: email}) //busco el mail en la base de datos
     .then(user => {
@@ -41,6 +43,7 @@ app.post('/login', (req, res) => {
     });
 });
 
+//app.post este codigo se ejecuta cuando recibo una solicitud post en mi puerto 3001 en la ruta /register
 app.post('/register', (req, res) => {
     const { email, password } = req.body; //guardo lo que me manda el cliente
 
--- challenge/backend/models/Table.js
+++ challenge/backend/models/Table.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose')
 
-const UserSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({ //es como una plantilla, la forma que tendran los datos almacenados en la coleccion UserSchema. 
     email: String,
     password: String
 })
 
-const UserModel = mongoose.model("user", UserSchema)
+const UserModel = mongoose.model("user", UserSchema) //crea un modeo basado en el esuema UserSchema, el primer argumento es el nombre de la coleccion en la base de datos.
+                                                     //el segundo define como sera el esquema.
 module.exports = UserModel
